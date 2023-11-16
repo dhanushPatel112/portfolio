76d77ad2dbf7e255f76c8540e9df73fd632c7372
dhanush patel <dhanush.patel@kcsitglobal.com>
2023-11-16 14:40:55 +0530

    Optimize landing profile pic & removed unused loading

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,24 +1,23 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react"
 import Image from "next/image"
-import landingImage from "/public/images/profile 2.jpeg"
-import Loading from "./Loading"
+import landingImage from "/public/images/profile 2.jpg"
 
 const Landing = () => {
-    return (
-        <div className="landing-style-display-container landing-style-content landing-style-wide" id="home">
-            <Image className="landing-style-image" src={landingImage} alt="Profile Pic" placeholder="blur" />
-            <div className="landing-style-display-middle landing-style-margin-top landing-style-center">
-                <h1 className="landing-style-xxlarge landing-style-text-black">
-                    <span className="landing-style-padding landing-style-black landing-style-opacity-min">
-                        <b>Dhanush Patel</b>
-                    </span>
-                    <br />
-                    <span className="landing-style-hide-small landing-style-text-dark-grey">Software Developer</span>
-                </h1>
-            </div>
-        </div>
-    )
+  return (
+    <div className="landing-style-display-container landing-style-content landing-style-wide" id="home">
+      <Image className="landing-style-image" src={landingImage} alt="Profile Pic" placeholder="blur" />
+      <div className="landing-style-display-middle landing-style-margin-top landing-style-center">
+        <h1 className="landing-style-xxlarge landing-style-text-black">
+          <span className="landing-style-padding landing-style-black landing-style-opacity-min">
+            <b>Dhanush Patel</b>
+          </span>
+          <br />
+          <span className="landing-style-hide-small landing-style-text-dark-grey">Software Developer</span>
+        </h1>
+      </div>
+    </div>
+  )
 }
 
 export default Landing
