e7dc4a6bbf4ee4aa79e73e5afdd1301098bc3cc5
dhanush <pateldhanush1208@gmail.com>
2025-09-20 13:26:22 +0530

    feat: speed analytics & fix: next build

--- next.config.js
+++ next.config.js
@@ -2,11 +2,4 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  // output: "export",
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
-  },
-  basePath: isProd ? "/portfolio" : "",
-  assetPrefix: isProd ? "/portfolio/" : "",
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -6,6 +8,7 @@ export default function Document() {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" />
       </Head>
       <body>
+        <SpeedInsights/>
         <Main />
         <NextScript />
       </body>
